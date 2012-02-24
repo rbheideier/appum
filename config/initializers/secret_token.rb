f36fab69a96cab851b3179b09944df723de3a42c
Raphael Heideier <rbheideier@yahoo.com.br>
2012-02-24 16:05:41 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appum::Application.config.secret_token = 'ccc2e2eb6b6dffbd584de356d6758d830aaa72228d1fc30356715348f551ff72cc5225920aef2ac9a1401cc465a5803dbe562859b374ebe14efeac9b521daa14'
